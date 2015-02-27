0ebe4f1cd01a7cd44a2566044bb05e8fea9dbad4
Cenk Bircanoglu <cenk.bircanoglu@gmail.com>
2015-02-27 10:05:54 +0200

    daily rollups for clean up

--- cronjob/app/clean_up.py
+++ cronjob/app/clean_up.py
@@ -14,7 +14,12 @@ logger = get_logger()
 
 def clean_up(args):
     keyspace = args[1]
-    start_time = int(round(int(args[2]) / 100) * 100)
+
+    date_value = datetime.datetime.fromtimestamp(int(args[2]))
+
+    start_time = datetime.datetime(date_value.year, date_value.month, date_value.day, date_value.hour, 00)
+    start_time = int(start_time.strftime("%s"))
+
     end_time = start_time + (60 * 60)
     task_end = int(datetime.datetime.now().strftime("%s"))
     while end_time < task_end:
@@ -38,7 +43,33 @@ def clean_up(args):
         time.sleep(300)
 
 
-# clean_up("")
+    date_value = datetime.datetime.fromtimestamp(int(args[2]))
+
+    start_time = datetime.datetime(date_value.year, date_value.month, date_value.day, date_value.hour, 00)
+    start_time = int(start_time.strftime("%s"))
+
+    end_time = start_time + (60 * 60 *24)
+    while end_time < task_end:
+        logger.debug("End Time: %s, Start Time: %s", end_time, start_time)
+        command = "nohup /data/spark/bin/spark-submit --class net.egemsoft.rrd.Main  " \
+                  "--master spark://ipam-ulus-db-2  target/cassandra-spark-rollup-1.0-driver.jar " \
+                  " spMaster=spark://ipam-ulus-db-2:7077 casHost=ipam-ulus-db-1 " \
+                  "casKeyspace=%s casTable=metric rollup=3600 start=%s end=%s destRollup=86400 ttl=94608000  &\n" % (
+                      keyspace, start_time, end_time)
+        logger.debug("Command: %s", command)
+        try:
+            p = subprocess.call(command, shell=True, stdout=subprocess.PIPE,
+                                cwd="/home/sparkuser/cassandra-spark-rollup")
+            logger.debug(p)
+
+        except Exception, e:
+            pass
+            logger.error(e.message)
+        start_time = start_time + (60 * 60 *24)
+        end_time = start_time + (60 * 60 *24)
+        time.sleep(1000)
+
+
 clean_up(sys.argv)
 
 ####usage
